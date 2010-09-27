19fd60a0abb22ca819c4cceaefbbedd114043378
ewheeler <evanmwheeler@gmail.com>
2010-09-27 18:41:24 -0400

    initial forecast plot

--- vaxapp/views.py
+++ vaxapp/views.py
@@ -19,6 +19,10 @@ def index(req):
         {"countrystocks": CountryStock.objects.all()})
 
 def chart_country(req, country_pk=None, vaccine_abbr=None):
+    # configuration options
+    save_charts = False
+    display_buffers = True
+
     country_stock = CountryStock.objects.get(country__pk=country_pk,\
         vaccine__abbr=vaccine_abbr)
 
@@ -34,31 +38,42 @@ def chart_country(req, country_pk=None, vaccine_abbr=None):
     try:
         forecasts = country_stock.forecast_set.all().order_by('year')
 
-        three_by_year = dict()
-        nine_by_year = dict()
+        if display_buffers:
+            three_by_year = dict()
+            nine_by_year = dict()
 
-        # create dicts mapping year to buffer levels
-        # (according to CO forecast annual demand estimate)
-        three = [three_by_year.update({f.year:f.three_month_buffer}) for f in forecasts ]
-        nine = [nine_by_year.update({f.year:f.nine_month_buffer}) for f in forecasts ]
+            # create dicts mapping year to buffer levels
+            # (according to CO forecast annual demand estimate)
+            three = [three_by_year.update({f.year:f.three_month_buffer}) for f in forecasts ]
+            nine = [nine_by_year.update({f.year:f.nine_month_buffer}) for f in forecasts ]
 
-        # make a list of corresponding buffer levels for every reported stock level
-        three_month_buffers = [three_by_year[d.year] for d in dates]
-        nine_month_buffers = [nine_by_year[d.year] for d in dates]
+            # make a list of corresponding buffer levels for every reported stock level
+            # TODO can this be done without plotting for each stock level?
+            three_month_buffers = [three_by_year[d.year] for d in dates]
+            nine_month_buffers = [nine_by_year[d.year] for d in dates]
+
+        deliveries = country_stock.delivery_set.all().order_by('date')
+        future_forecast = deliveries.filter(type='FF')
+        future_forecast_dates = future_forecast.values_list('date', flat=True)
+        future_forecast_amounts = future_forecast.values_list('amount', flat=True)
 
     except Exception, e:
+        print 'ERROR DATA'
         print e
 
     try:
-        fig = figure()
+        fig = figure(figsize=(9,6))
         ax = fig.add_subplot(111)
 
         # plot stock levels
         ax.plot_date(dates, levels, '-', drawstyle='steps')
 
-        # plot 3 and 9 month buffer levels as red lines
-        ax.plot_date(dates, three_month_buffers, '-', drawstyle='steps', color='red')
-        ax.plot_date(dates, nine_month_buffers, '-', drawstyle='steps', color='red')
+        if display_buffers:
+            # plot 3 and 9 month buffer levels as red lines
+            ax.plot_date(dates, three_month_buffers, '-', drawstyle='steps', color='red')
+            ax.plot_date(dates, nine_month_buffers, '-', drawstyle='steps', color='red')
+
+        ax.plot_date(future_forecast_dates, future_forecast_amounts, '-', drawstyle='steps', color='green')
 
         # format the ticks
         ax.xaxis.set_major_locator(years)
@@ -76,9 +91,9 @@ def chart_country(req, country_pk=None, vaccine_abbr=None):
         matplotlib.pyplot.close(fig)
 
     except Exception, e:
+        print 'ERROR FIGURE'
         print e
 
-    save_charts = False
     if save_charts:
         filename = "%s-%s-%s.png" % (datetime.datetime.today().date().isoformat(),\
             country_pk, vaccine_abbr)
