ed6ddc371ab1c390be632aab05a8fca458ba9984
ewheeler <evanmwheeler@gmail.com>
2011-06-03 22:50:57 +0000

    try flashcanvas instead of excanvas

--- vaxapp/urls.py
+++ vaxapp/urls.py
@@ -18,6 +18,8 @@ urlpatterns = patterns('',
     # serve assets via django, during development
     url(r'^assets/js/(?P<path>.*)$', "django.views.static.serve",
     {"document_root": os.path.dirname(__file__) + "/static/javascripts"}),
+    url(r'^assets/flashcanvas/(?P<path>.*)$', "django.views.static.serve",
+    {"document_root": os.path.dirname(__file__) + "/static/flashcanvas"}),
     url(r'^assets/css/(?P<path>.*)$', "django.views.static.serve",
     {"document_root": os.path.dirname(__file__) + "/static/stylesheets"}),
     url(r'^assets/images/(?P<path>.*)$', "django.views.static.serve",
